5a9d30d5f0a7cfd02c955b2a65aa1c7ebe6210fb
khanmaw-cmd <khanmaw@gmail.com>
2026-09-24 10:17:49 +0530

    Add shared print button for receipts

---
+++ components/print-button.tsx
@@ -0,0 +1,1 @@
+"use client";export default function PrintButton(){return <button type="button" onClick={()=>window.print()} className="rounded-xl border bg-white px-4 py-2 text-sm print:hidden">Print receipt</button>}
